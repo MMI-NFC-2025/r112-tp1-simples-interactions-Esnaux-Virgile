971d976a9739571d646c61e1867c81d5a2df5096
Esnaux-Virgile <virgile.esnaux@edu.univ-fcomte.fr>
2025-11-07 10:54:39 +0100

    fin d'exercice

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // TODO
+
+// Changer la couleur du texte
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color = "red";
+
+// Réagir à un click
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt)=>{
+    enRougeSuiteAClick.style.color = "red";
+});
+
+// Trouvez les 'H2' du document
+const collH2 = document.querySelectorAll("h2")
+collH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{evt.target.style.color = "red"; }); });
